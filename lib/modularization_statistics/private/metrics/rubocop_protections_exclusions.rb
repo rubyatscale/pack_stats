2510fa318f70af7a05660de311a464ed057ea79d
Alex Evanczuk <alex.evanczuk@gusto.com>
2022-09-07 10:09:17 -0400

    Add observability into "violations" for rubocop based package protections (#9)
    * add failing tests
    
    * fix specs
    
    * bump version

---
+++ lib/modularization_statistics/private/metrics/rubocop_protections_exclusions.rb
@@ -0,0 +1,34 @@
+# typed: strict
+# frozen_string_literal: true
+
+module ModularizationStatistics
+  module Private
+    module Metrics
+      class RubocopProtectionsExclusions
+        extend T::Sig
+
+        sig { params(prefix: String, protected_packages: T::Array[PackageProtections::ProtectedPackage], package_tags: T::Array[Tag]).returns(T::Array[GaugeMetric]) }
+        def self.get_rubocop_exclusions(prefix, protected_packages, package_tags)
+          rubocop_based_package_protections = T.cast(PackageProtections.all.select { |p| p.is_a?(PackageProtections::RubocopProtectionInterface) }, T::Array[PackageProtections::RubocopProtectionInterface])
+          rubocop_based_package_protections.flat_map do |rubocop_based_package_protection|
+            metric_name = "#{prefix}.#{rubocop_based_package_protection.identifier}.rubocop_exclusions.count"
+            all_exclusions_count = ParsePackwerk.all.sum { |package| exclude_count_for_package_and_protection(package, rubocop_based_package_protection)}
+            GaugeMetric.for(metric_name, all_exclusions_count, package_tags)
+          end
+        end
+
+        sig { params(package: ParsePackwerk::Package, protection: PackageProtections::RubocopProtectionInterface).returns(Integer) }
+        def self.exclude_count_for_package_and_protection(package, protection)
+          rubocop_todo = package.directory.join('.rubocop_todo.yml')
+          if rubocop_todo.exist?
+            loaded_rubocop_todo = YAML.load_file(rubocop_todo)
+            cop_config = loaded_rubocop_todo.fetch(protection.cop_name, {})
+            cop_config.fetch('Exclude', []).count
+          else
+            0
+          end
+        end
+      end
+    end
+  end
+end
