27a086a75b8aab1b57a308bbc0ceacccb1bd3cde
Alex Evanczuk <alex.evanczuk@gusto.com>
2022-11-15 08:31:09 -0500

    Bump package_protections and rubocop-packs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -111,7 +111,6 @@ def write_package_yml(
     'prevent_other_packages_from_using_this_packages_internals' => 'fail_on_new',
     'prevent_this_package_from_exposing_an_untyped_api' => 'fail_on_new',
     'prevent_this_package_from_creating_other_namespaces' => 'fail_on_new',
-    'prevent_other_packages_from_using_this_package_without_explicit_visibility' => 'fail_never',
   }
   protections_with_defaults = defaults.merge(protections)
   metadata = { 'protections' => protections_with_defaults }
