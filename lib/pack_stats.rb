c7aea717ce7db8cebc7b0d6dd674395a44d63342
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-05-05 23:36:37 -0400

    Remove use_gusto_legacy_names
    We are going to move to using the currently supported public API.

--- lib/pack_stats.rb
+++ lib/pack_stats.rb
@@ -39,8 +39,6 @@ module PackStats
       # See note on get_metrics
       packaged_source_code_locations: T.nilable(T::Array[Pathname]),
       # See note on get_metrics
-      use_gusto_legacy_names: T::Boolean,
-      # See note on get_metrics
       max_enforcements_tag_value: T::Boolean
     ).void
   end
@@ -52,7 +50,6 @@ module PackStats
     report_time: Time.now, # rubocop:disable Rails/TimeZone
     verbose: false,
     packaged_source_code_locations: [],
-    use_gusto_legacy_names: false,
     max_enforcements_tag_value: false
   )
 
@@ -60,7 +57,6 @@ module PackStats
       source_code_pathnames: source_code_pathnames,
       componentized_source_code_locations: componentized_source_code_locations,
       app_name: app_name,
-      use_gusto_legacy_names: use_gusto_legacy_names,
       max_enforcements_tag_value: max_enforcements_tag_value,
     )
 
@@ -69,10 +65,6 @@ module PackStats
       all_metrics.each do |metric|
         puts "Sending metric: #{metric}"
       end
-    end
-
-    if use_gusto_legacy_names
-      all_metrics = Private.convert_metrics_to_legacy(all_metrics)
     end
 
     Private::DatadogReporter.report!(
@@ -89,11 +81,6 @@ module PackStats
       app_name: String,
       # This field is deprecated
       packaged_source_code_locations: T.nilable(T::Array[Pathname]),
-      # It is not recommended to set this to true.
-      # Gusto uses this to preserve historical trends in Dashboards as the names of
-      # things changed, but new dashboards can use names that better match current tooling conventions.
-      # The behavior of setting this parameter to true might change without warning
-      use_gusto_legacy_names: T::Boolean,
       # You can set this to `true` to tag all metrics with `max_enforcements:true`.
       # This is useful if you want to submit two sets of metrics:
       # Once with the violation counts as configured in the app
@@ -106,25 +93,18 @@ module PackStats
     componentized_source_code_locations:,
     app_name:,
     packaged_source_code_locations: [],
-    use_gusto_legacy_names: false,
     max_enforcements_tag_value: false
   )
 
     GaugeMetric.set_max_enforcements_tag(max_enforcements_tag_value)
 
-    all_metrics = Private::DatadogReporter.get_metrics(
+    Private::DatadogReporter.get_metrics(
       source_code_files: source_code_files(
         source_code_pathnames: source_code_pathnames,
         componentized_source_code_locations: componentized_source_code_locations,
       ),
       app_name: app_name
     )
-
-    if use_gusto_legacy_names
-      all_metrics = Private.convert_metrics_to_legacy(all_metrics)
-    end
-
-    all_metrics
   end
 
   sig do
